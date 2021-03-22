5f3b803db9ea5ff1282bb9c74b7e28c0aef8351a
Kenia Chang He <keniachang1997@gmail.com>
2021-03-22 02:47:10 -0400

    modify checkout button in cart

--- pages/cart.js
+++ pages/cart.js
@@ -80,13 +80,15 @@ const cart = () => {
                             <p>&ensp;${total()}</p>
                         </div>
                         {/* Checkout button */}
-                        <div className='text-center'>
-                            <button className='bg-green-500 py-2 px-4 rounded shadow text-lg text-white hover:bg-green-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50'>
-                                <Link href='/checkout'>
-                                    <a>Checkout</a>
-                                </Link>
-                            </button>
-                        </div>
+                        {(cartItems && cartItems.length) ? (
+                            <div className='text-center'>
+                                <button className='bg-green-500 py-2 px-4 rounded shadow text-lg text-white hover:bg-green-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50'>
+                                    <Link href='/checkout'>
+                                        <a>Checkout</a>
+                                    </Link>
+                                </button>
+                            </div>
+                        ) : null}
                     </div>
                 </div>
             </div>
