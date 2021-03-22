bf63859b5bfd2d97adcee950966ae23b989fecd0
Kenia Chang He <keniachang1997@gmail.com>
2021-03-22 02:52:53 -0400

    update production domain

--- config/index.js
+++ config/index.js
@@ -1,2 +1,2 @@
 const dev = process.env.NODE_ENV !== 'production';
-export const server = dev ? 'http://localhost:3000' : 'https://mywebsite.com';
+export const server = dev ? 'http://localhost:3000' : 'https://comegg.vercel.app';
